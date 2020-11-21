fbd2fe4ca78ca21a7a54a789191eefd0db4d2662
Batuhan Göbekli <btubatuhan@gmail.com>
2020-11-21 17:16:52 +0300

    Initial project added.

---
+++ GetTodo/GetTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetTodo
+//
+//  Created by Batuhan Göbekli on 21.11.2020.
+//  Copyright © 2020 Batuhan Göbekli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
